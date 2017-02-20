588b5cf5d6e20d50c6f502417979d203f13ad1e3
Aaron1515 <aaron1515@gmail.com>
2017-02-19 18:37:05 -0800

    create association for User to many interests

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-   
+
+         has_many :interests
 end
+
+
+
+
